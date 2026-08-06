984641cf1279ee7ba590ea1ea30e1f0a89de1fba
MARIA THERESA C PENASO <penasomariatheresa26@gmail.com>
2026-08-06 12:10:06 +0800

    Upload all SIA2 and App Dev activities

--- FINAL_LARAVEL_SYSTEM/resources/views/components/application-logo.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="flex flex-col items-center justify-center">
-    <div class="flex h-16 w-16 items-center justify-center rounded-2xl bg-pink-600 text-4xl text-white shadow-lg shadow-pink-200">
-        ❀
-    </div>
-    <h1 class="mt-3 text-2xl font-black text-pink-700">Bloomery</h1>
-    <p class="text-xs font-medium text-pink-400">Flower Shop</p>
-</div>
